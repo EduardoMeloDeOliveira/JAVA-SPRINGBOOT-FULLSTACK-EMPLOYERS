46ccb538a48e4ff31f97a58de6e3bd5efd789a7f
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2024-10-19 19:59:29 -0300

    finaliza o header e o footer, pré configuração do react-router-dom

--- front-end/ems-frontend/src/components/FooterComponent.jsx
+++ front-end/ems-frontend/src/components/FooterComponent.jsx
@@ -5,7 +5,7 @@ const FooterComponent = () => {
         <div>
             <footer className='bg-black text-white text-center py-3'>
                 <p>
-                    Desenvolvido por Eduardo Melo &copy; {new Date().getFullYear()} - Todos os direitos reservados.
+                    Desenvolvido por <a href="https://github.com/EduardoMeloDeOliveira" target='blank'>Eduardo Melo de Oliveira</a>&copy; {new Date().getFullYear()} - Todos os direitos reservados.
                 </p>
 
             </footer>
--- front-end/ems-frontend/src/components/HeaderComponent.jsx
+++ front-end/ems-frontend/src/components/HeaderComponent.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const HeaderComponent = () => {
-  return (
-    <div>
+    return (
+        <div>
 
-        <header>
-            <nav className= 'navbar-dark bg-black h-25 py-3 d-flex justify-content-center align-items-center mb-3'>
-            <a className='navbar-brand fs-4 ' href='https://github.com/EduardoMeloDeOliveira'>Employee Management system</a>
-            </nav>
-        </header>
-    </div>
-  )
+            <header>
+                <nav className='navbar-dark bg-black h-25 py-3 d-flex justify-content-center align-items-center mb-3'>
+                    <a className='navbar-brand fs-4 ' href='https://github.com/EduardoMeloDeOliveira' target='blank'>Employee Management system</a>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default HeaderComponent
