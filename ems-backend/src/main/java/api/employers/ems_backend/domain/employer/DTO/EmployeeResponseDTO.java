05d7c54cead7a38f89dd4906a5c832c50c7b7a41
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2024-10-19 18:51:56 -0300

    atualiza a config de cors e atualiza o nome que estava incorreto no response

--- ems-backend/src/main/java/api/employers/ems_backend/domain/employer/DTO/EmployeeResponseDTO.java
+++ ems-backend/src/main/java/api/employers/ems_backend/domain/employer/DTO/EmployeeResponseDTO.java
@@ -2,7 +2,7 @@ package api.employers.ems_backend.domain.employer.DTO;
 
 import api.employers.ems_backend.domain.employer.entity.Employee;
 
-public record EmployeeResponseDTO (Long id, String fistName, String lastName, String email){
+public record EmployeeResponseDTO (Long id, String firstName, String lastName, String email){
 
     public EmployeeResponseDTO (Employee employee){
         this(employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getEmail());
