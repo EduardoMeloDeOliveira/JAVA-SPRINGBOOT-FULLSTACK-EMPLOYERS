3c998478cdbaeb72560be53af65ef4d61057ca40
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2024-10-19 18:37:57 -0300

    cria table de employees

--- front-end/ems-frontend/src/App.jsx
+++ front-end/ems-frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import HelloWorld from './HelloWorld'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
   
 
   return (
-    <>
-      <HelloWorld></HelloWorld>
-    </>
+   
+
+    <ListEmployeeComponent/>
+   
   )
 }
 
